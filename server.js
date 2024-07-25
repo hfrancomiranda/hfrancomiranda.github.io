b0f4e5afbbb1d59451272b157bcff7137b0f1063
hfranco <145460275+hfrancomiranda@users.noreply.github.com>
2024-07-24 21:06:12 -0400

    Update server.js
    modified port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 5433;
+const port = process.env.port || 5433;
 
 // Configure PostgreSQL connection
 const pool = new Pool({
